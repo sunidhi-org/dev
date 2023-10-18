9e75506f0287164b31ab3a6737c54206819a2849
Sukriti S <eng21ct0039@dsu.edu.in>
2023-10-18 19:30:31 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,25 +1,10 @@
-# Program to display the Fibonacci sequence up to n-th term
+# This program adds two numbers
 
-nterms = int(input("How many terms? "))
+num1 = 1.5
+num2 = 6.3
 
-# first two terms
-n1, n2 = 0, 1
-count = 0
+# Add two numbers
+sum = num1 + num2
 
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Please enter a positive integer")
-# if there is only one term, return n1
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
-# generate fibonacci sequence
-else:
-   print("Fibonacci sequence:")
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
