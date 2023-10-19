df0ff4ddd256d580e4b832acf8f510e69692c7bf
Sukriti S <eng21ct0039@dsu.edu.in>
2023-10-19 12:58:07 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,19 +1,10 @@
-# Python program to find the factorial of a number provided by the user.
+# This program adds two numbers
 
-# change the value for a different result
-num = 7
+num1 = 1.5
+num2 = 6.3
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+# Add two numbers
+sum = num1 + num2
 
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
